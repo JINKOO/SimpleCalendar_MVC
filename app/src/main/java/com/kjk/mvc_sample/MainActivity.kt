8b6ee1f670ed77c0e248a5502875bc46b770be6a
JINKOO <jacob777300@gmail.com>
2021-11-23 23:49:57 +0900

    viewBinding 추가. CalendarView 추가.

--- app/src/main/java/com/kjk/mvc_sample/MainActivity.kt
+++ app/src/main/java/com/kjk/mvc_sample/MainActivity.kt
@@ -2,10 +2,20 @@ package com.kjk.mvc_sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kjk.mvc_sample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        initLayoutValues()
+    }
+
+    private fun initLayoutValues() {
+        setContentView(binding.root)
     }
 }
