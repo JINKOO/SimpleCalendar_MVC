7df58bf70da36f83a1c91bdea66883ba3117a7d3
JINKOO <jacob777300@gmail.com>
2021-11-28 19:33:08 +0900

    어댑터 추가

--- app/src/main/java/com/kjk/mvc_sample/view/CalendarAdapter.kt
+++ app/src/main/java/com/kjk/mvc_sample/view/CalendarAdapter.kt
@@ -1,4 +1,24 @@
 package com.kjk.mvc_sample.view
 
-class CalendarAdapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class CalendarAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        var binding =
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+        return 0
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
 }
